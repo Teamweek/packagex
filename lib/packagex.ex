62c15d135b1c3a63d7ea03e91e674c2423c3b608
Artur Cygan <arczicygan@gmail.com>
2017-01-31 13:25:50 +0100

    Empty project

---
+++ lib/packagex.ex
@@ -0,0 +1,18 @@
+defmodule Packagex do
+  @moduledoc """
+  Documentation for Packagex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Packagex.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/packagex_test.exs
@@ -0,0 +1,8 @@
+defmodule PackagexTest do
+  use ExUnit.Case
+  doctest Packagex
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
